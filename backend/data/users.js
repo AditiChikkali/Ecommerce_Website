29cd766f6db2c77c7a0909a14033604a94e35eca
Aditi chikkali <chikkali.a@northeastern.edu>
2023-06-08 17:16:19 -0400

    adding production changes

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,27 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 
 const users = [
-    {
-        name: "Admin User",
-        email: "admin@example.com",
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: true
-    },
-    {
-        name: "Rutuja Ghogare",
-        email: "rutuja@example.com",
-        password: bcrypt.hashSync('123456', 10),
-    },
-    {
-        name: "Aditi Chikkalli",
-        email: "aditi@example.com",
-        password: bcrypt.hashSync('123456', 10),
-    },
-]
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Rutuja Ghogare',
+    email: 'rutuja@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Aditi Chikkalli',
+    email: 'aditi@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Aditi ',
+    email: 'newadmin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
 
 export default users;
